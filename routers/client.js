d03f85cdeaac1cf33bbcabadf48164b80e8e8ab6
lethiwe <lethi.nks30@gmail.com>
2021-02-04 16:06:58 +0200

    updated project

--- routers/client.js
+++ routers/client.js
@@ -39,6 +39,46 @@ router.post('/addcart',(req,res)=>{
       })
 })
 
+//insert to payment
+
+router.post('/payment',(req,res)=>{
+    
+    adminId = connection.conn.query('SELECT admin_id FROM admin', function(res,err,result){
+        if(err)
+        {throw err;
+          console.log("[mysql error]",err)
+        }
+        else{
+            if(result.length > 0)
+            {
+                res.send(result);
+           }
+        }
+
+      });
+    customerId = connection.conn.query('SELECT customer_id FROM customers', function(res,err, result){if(err) throw err;
+    res.send(result);
+    });
+    cartId= connection.conn.query('SELECT cart_id FROM cart');
+    // totalPrice = req.body.totalPrice;
+    // cust_name = req.body.name;
+    // email =req.body.email;
+
+    cpay='INSERT INTO payment_tbl(totalPrice,id_customer,id_admin,id_cart,customer_name,customer_email) VALUES (?,?,?,?,?,?)';
+    customerValue =[req.body.totalPrice,customerId,adminId,cartId,req.body.name,req.body.email];
+    connection.conn.query(cpay,[customerValue],(error,rows)=>{
+        if(!error)
+        {
+            res.send(rows);
+        }else
+
+        {
+            console.log(error);
+        }
+    })
+
+})
+
 
 //payment  NOT FUNCTIONALLY STILL WORKING ON IT
 router.put('/cpayment/:id',(req,res)=>{
